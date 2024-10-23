ceb960ecddf9b3258e444576ddad3f2c06692cd3
unknown <berkezorlu@gmail.com>
2024-10-23 08:56:10 +0200

    updated !standings to match latest website

--- bot.py
+++ bot.py
@@ -217,7 +217,7 @@ async def standings(ctx):
     for i in range(1, 11):
         if(len(standingsTable) <= i):
             break
-        s += standingsTable[i][0] + ". " + standingsTable[i][1] + ": " + standingsTable[i][len(standingsTable[i]) - 1] + "\n" #+ " (" + standingsTable[i][len(standingsTable[i]) - 1] + ")\n"
+        s += standingsTable[i][0] + ". " + standingsTable[i][1] + ": " +standingsTable[i][len(standingsTable[i]) - 1] + "\n"
     s += "\n"
     
     session = HTMLSession()
